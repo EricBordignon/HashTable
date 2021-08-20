96c339d2c5d5651a1eb7a57a1b72d34a09fd3ba3
EricBordignon <ericj.bordignon@gmail.com>
2021-08-20 19:28:48 +0000

    Initial commit

---
+++ hashTable.c
@@ -0,0 +1,36 @@
+#include "hashTable.h"
+
+
+
+int main(void)
+{
+    // Set all next to NULL
+    /*for(int i = 0; i < SIZE; i++)
+    {
+        DICTIONARY[i] -> next = NULL;
+        strcpy(DICTIONARY[i] -> word, "unknown");
+    }*/
+
+    int keep = 1;
+
+    // Get words
+    while(keep)
+    {
+        insertion(get_string("What is the word: \n"));
+
+        keep = get_int("1. Add another number: \n0. Exit: \n");
+    }
+
+    // Print out words
+    for(int i = 0; i < SIZE; i++)
+    {
+        for(node *ptr = DICTIONARY[i]; ptr != NULL; ptr = ptr -> next)
+        {
+            printf("%s;  ", ptr -> word);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
